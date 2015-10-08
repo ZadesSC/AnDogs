6051f551c7467c1b1b5a305861d9d0534fd3e2da
Darren Cheng <zadespu@gmail.com>
2015-10-07 18:04:19 -0700

    initial commit

---
+++ android/src/io/zades/andogs/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.zades.andogs.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.zades.andogs.AnDogs;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AnDogs(), config);
+	}
+}
